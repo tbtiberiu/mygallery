b5164f5f7cb7f84aeef1b8d697a639e7720999e7
Tiberiu Ioan <39851604+tbtiberiu@users.noreply.github.com>
2022-09-11 11:15:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,11 +61,6 @@ resource "azurerm_linux_virtual_machine" "mygallery" {
     azurerm_network_interface.mygallery.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
